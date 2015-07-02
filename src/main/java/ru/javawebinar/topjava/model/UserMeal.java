cdb51e7300cf230c7b24afdf0b13bede8241d88f
Volodymyr <elginior@gmail.com>
2015-07-02 19:49:38 +0300

    My HW4

--- src/main/java/ru/javawebinar/topjava/model/UserMeal.java
+++ src/main/java/ru/javawebinar/topjava/model/UserMeal.java
@@ -1,21 +1,48 @@
 package ru.javawebinar.topjava.model;
 
-import javax.persistence.FetchType;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
 /**
  * GKislin
  * 11.01.2015.
  */
+@Entity
+@Table(name = "MEALS")
+@NamedQueries(value = {
+        @NamedQuery(name = UserMeal.UPDATE, query = "UPDATE UserMeal SET dateTime=:dateTime,description=:description,calories=:calories " +
+                "WHERE id=:id AND user.id=:user_id"),
+        @NamedQuery(name = UserMeal.DELETE, query = "DELETE FROM UserMeal m WHERE m.id=:id AND m.user.id=:user_id"),
+        @NamedQuery(name = UserMeal.GET, query = "SELECT m FROM UserMeal m WHERE m.id=:id AND m.user.id=:user_id"),
+        @NamedQuery(name = UserMeal.ALL_DELETE_BY_USER, query = "DELETE FROM UserMeal m WHERE m.user.id=:user_id"),
+        @NamedQuery(name = UserMeal.ALL_SORTED_BY_USER, query = "SELECT m FROM UserMeal m WHERE m.user.id=:user_id ORDER BY m.dateTime desc"),
+        @NamedQuery(name = UserMeal.INTERVAL_SORTED_BY_USER, query = "SELECT m FROM UserMeal m WHERE m.user.id=:user_id " +
+                "AND m.dateTime>=:start AND m.dateTime<=:end ORDER BY m.dateTime desc"),
+})
 public class UserMeal extends BaseEntity{
+
+    public static final String UPDATE = "UserMeal.update";
+    public static final String DELETE = "UserMeal.delete";
+    public static final String GET = "UserMeal.getById";
+    public static final String ALL_DELETE_BY_USER = "UserMeal.deleteAll";
+    public static final String ALL_SORTED_BY_USER = "UserMeal.getAllByUser";
+    public static final String INTERVAL_SORTED_BY_USER = "UserMeal.getIntervalByUser";
+
+
+
+    @Column(name = "datetime")
     protected LocalDateTime dateTime;
 
+    @Column(name = "description")
     protected String description;
 
+    @Column(name = "calories")
     protected int calories;
 
+    @JoinColumn(name = "user_id")
     @ManyToOne(fetch = FetchType.LAZY)
+    @NotNull
     private User user;
 
     public UserMeal() {
--- src/main/java/ru/javawebinar/topjava/repository/jpa/JpaUserMealRepositoryImpl.java
+++ src/main/java/ru/javawebinar/topjava/repository/jpa/JpaUserMealRepositoryImpl.java
@@ -1,9 +1,14 @@
 package ru.javawebinar.topjava.repository.jpa;
 
+import org.springframework.dao.support.DataAccessUtils;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.model.UserMeal;
 import ru.javawebinar.topjava.repository.UserMealRepository;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -13,35 +18,60 @@ import java.util.List;
  */
 
 @Repository
+@Transactional(readOnly = true)
 public class JpaUserMealRepositoryImpl implements UserMealRepository {
 
+    @PersistenceContext
+    private EntityManager em;
+
     @Override
+    @Transactional
     public UserMeal save(UserMeal userMeal, int userId) {
-        return null;
+        if (userMeal.isNew()) {
+            User ref = em.getReference(User.class,userId);
+            userMeal.setUser(ref);
+            em.persist(userMeal);
+        } else {
+            if (em.createNamedQuery(UserMeal.UPDATE)
+                    .setParameter("dateTime",userMeal.getDateTime())
+                    .setParameter("description",userMeal.getDescription())
+                    .setParameter("calories",userMeal.getCalories())
+                    .setParameter("id",userMeal.getId())
+                    .setParameter("user_id", userId)
+                    .executeUpdate() == 0) return null;
+        }
+        return userMeal;
     }
 
     @Override
+    @Transactional
     public boolean delete(int id, int userId) {
-        return false;
+        return em.createNamedQuery(UserMeal.DELETE).setParameter("id",id).setParameter("user_id",userId).executeUpdate() != 0;
     }
 
     @Override
     public UserMeal get(int id, int userId) {
-        return null;
+        List<UserMeal> list = em.createNamedQuery(UserMeal.GET,UserMeal.class).setParameter("id",id).setParameter("user_id",userId).getResultList();
+        return DataAccessUtils.singleResult(list);
     }
 
     @Override
     public List<UserMeal> getAll(int userId) {
-        return null;
+        return em.createNamedQuery(UserMeal.ALL_SORTED_BY_USER,UserMeal.class).setParameter("user_id",userId).getResultList();
     }
 
     @Override
+    @Transactional
     public void deleteAll(int userId) {
-
+        em.createNamedQuery(UserMeal.ALL_DELETE_BY_USER).setParameter("user_id",userId).executeUpdate();
     }
 
     @Override
     public List<UserMeal> getBetween(LocalDateTime startDate, LocalDateTime endDate, int userId) {
-        return null;
+        return em.createNamedQuery(UserMeal.INTERVAL_SORTED_BY_USER,UserMeal.class)
+                .setParameter("user_id",userId)
+                .setParameter("start",startDate)
+                .setParameter("end",endDate)
+                .getResultList();
     }
 }
