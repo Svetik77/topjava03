b41ec6a1b31ffc23bb1c18b577990b0d8bbc91cc
Elginior <elginior@gmail.com>
2015-08-21 10:14:51 +0300

    Filnal lesson

--- src/main/java/ru/javawebinar/topjava/web/user/AdminAjaxController.java
+++ src/main/java/ru/javawebinar/topjava/web/user/AdminAjaxController.java
@@ -1,13 +1,13 @@
 package ru.javawebinar.topjava.web.user;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.support.SessionStatus;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.to.UserTo;
 import ru.javawebinar.topjava.util.UserUtil;
+import ru.javawebinar.topjava.util.exception.ValidationException;
 
 import javax.validation.Valid;
 import java.util.List;
@@ -25,8 +25,8 @@ public class AdminAjaxController extends AbstractUserController {
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public User get(@PathVariable("id") int id) {
-        return super.get(id);
+    public UserTo getTo(@PathVariable("id") int id) {
+        return UserUtil.asTo(super.get(id));
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
@@ -35,19 +35,16 @@ public class AdminAjaxController extends AbstractUserController {
     }
 
     @RequestMapping(method = RequestMethod.POST)
-    public ResponseEntity<String> createOrUpdate(@Valid UserTo userTo, BindingResult result) {
+    public void createOrUpdate(@Valid UserTo userTo, BindingResult result, SessionStatus status) {
         if (result.hasErrors()) {
-            // TODO change to exception handler
-            StringBuilder sb = new StringBuilder();
-            result.getFieldErrors().forEach(fe -> sb.append(fe.getField()).append(" ").append(fe.getDefaultMessage()).append("<br>"));
-            return new ResponseEntity<>(sb.toString(), HttpStatus.UNPROCESSABLE_ENTITY);
+            throw new ValidationException(result);
         }
+        status.setComplete();
         if (userTo.getId() == 0) {
             super.create(UserUtil.createFromTo(userTo));
         } else {
             super.update(userTo);
         }
-        return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.POST)
